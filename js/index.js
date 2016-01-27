98a1a901eb3693799826c491434f28f972156ad6
Deltora72 <mohammad.j.dim@gmail.com>
2016-01-27 11:04:17 +0330

    Adding Ticket files

---
+++ js/index.js
@@ -0,0 +1,39 @@
+
+
+
+
+$('document').ready(function() {
+    
+$(".chosen-select").chosen({
+    no_results_text: "شهر مورد نظر یافت نشد."
+});
+
+    
+/*$.getJSON('js/database.json', function(data){
+    var m = data.place_name;
+    $('#suggestions-list').append('<li class="suggestion-item">'+m+'</li>');
+}); 
+    */
+    
+    
+});
+
+
+
+
+/*
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
+    
+    $http.get('js/database.json').success(function(data) {
+        $scope.cities = data;
+	}); 
+    
+}]);
+*/
+
+
+
+
+
